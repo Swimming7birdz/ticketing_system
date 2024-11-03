374720c02d1c8c718d39f0bcadcda1bda5668637
kevin <ktang2332@gmail.com>
2024-11-02 20:31:00 -0700

    remove unused pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import Login from './pages/Login/Login';
 import TicketSubmit from './pages/IssueSubmissionForm/IssueSubmissionForm';
 import TicketInfo from './pages/TicketInfo/TicketInfo';
@@ -17,8 +15,6 @@ function App() {
       <Route path="/login" element={<Login />} />
 
       <Route element={<NavBarLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/ticketinfo" element={<TicketInfo />} />
         <Route path="/ticketsubmit" element={<TicketSubmit />} />
         <Route path="/admindash" element={<AdminDash />} />
