476f62519a84a0495094c67ce8c6a1dc08980b35
kevin <ktang2332@gmail.com>
2024-11-03 01:55:46 -0700

    implemented ticket replies frontend

--- frontend/src/pages/TicketInfo/TicketInfo.js
+++ frontend/src/pages/TicketInfo/TicketInfo.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import TicketStatusIndicator from "../../components/TicketStatusIndicator/TicketStatusIndicator";
 import ConfirmDelete from "../../components/ConfirmDelete/ConfirmDelete";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ReplySection from "../../components/ReplySection/ReplySection";
 import './TicketInfo.css';
 
 const students = ["Kevin Tang", "Shabib Huq", "Rhea Mane", "Issac Alemu", "Ryan Radtke"]
@@ -12,6 +13,13 @@ const ProjectName = "Capstone Ticketing System"
 const ticketID = "CPSTN1-2345678"
 const TicketSubject = "Sponsor Isn’t Responding"
 const TicketDescription = "We’re having some communication issues with our project sponsor. Our sponsor missed a scheduled meeting with the team on Monday and has not responded to any email communication all week. We are unable to get our sprint document signed for this week because the sponsor is unresponsive. How should we approach this situation and can we get an extension on the sprint document this week?"
+
+const replyList = [
+    { id: 1, replyText: "Hi team, sorry to hear this. Has this happened before in the past?", author: "Jane Doe", time: "2 November 6:30pm" },
+    { id: 2, replyText: "No the sponsor is typically responsive but has not responded in a week.", author: "Kevin Tang", time: "2 November 7:30pm" },
+    { id: 3, replyText: "Ok I would recommend you to send a follow up. We will extend your deadline for the sprint retrospective this week.", author: "Jane Doe", time: "2 November 8:30pm" }
+]
+
 
 const TicketInfo = () => {
     const [deleteOpen, setDeleteOpen] = React.useState(false);
@@ -65,9 +73,10 @@ const TicketInfo = () => {
                     </Button>
                     <ConfirmDelete handleOpen={deleteOpen} handleClose={deletePopupClose}/>
                 </Stack>
-                Description:
+                <h3>Description:</h3>
                 <div className="ticketDescription">{TicketDescription}</div>
-                Replies:
+                <h3>Replies:</h3>
+                <ReplySection replies={replyList}></ReplySection>
             </Stack>
             <Stack className="ticketUsers">
                 <div>  
