2a6f7c7e426ce45e794e52e95296df1bb58e7da3
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2024-11-09 23:52:41 -0700

    integrating ticket queue with backend and improving issue form UI

--- frontend/src/pages/IssueSubmissionForm/IssueSubmissionForm.js
+++ frontend/src/pages/IssueSubmissionForm/IssueSubmissionForm.js
@@ -5,6 +5,7 @@ const IssueSubmissionForm = () => {
     const [studentName, setStudentName] = useState('');
     const [teamName, setTeamName] = useState('');
     const [sponsorName, setSponsorName] = useState('');
+    const [section, setSection] = useState('');
     const [instructorName, setInstructorName] = useState('');
     const [issueType, setIssueType] = useState('');
     const [description, setDescription] = useState('');
@@ -12,17 +13,18 @@ const IssueSubmissionForm = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        
+
         const submittedData = {
             studentName,
             teamName,
             sponsorName,
+            section,
             instructorName,
             issueType,
             description,
             teamMembers
         }
-        
+
         console.log(submittedData);
         alert('Ticket submitted successfully!');
 
@@ -30,6 +32,7 @@ const IssueSubmissionForm = () => {
         setStudentName('');
         setTeamName('');
         setSponsorName('');
+        setSection('');
         setInstructorName('');
         setIssueType('');
         setDescription('');
@@ -38,7 +41,7 @@ const IssueSubmissionForm = () => {
 
     return (
         <div className="container">
-            <h1 className='header'>Submit a Ticket For Project Capstone!</h1>
+            <h1 className='header'>Submit New Request</h1>
             <form onSubmit={handleSubmit}>
                 <label className='formLabel' htmlFor="studentName">Student Name:</label>
                 <input
@@ -70,6 +73,25 @@ const IssueSubmissionForm = () => {
                     required
                 />
 
+                <label className='formLabel' htmlFor="section">Class Section:</label>
+                <select
+                    id="section"
+                    className='formControl'
+                    value={section}
+                    onChange={(e) => setSection(e.target.value)}
+                    required
+                >
+                    <option value="" disabled>Select Section</option>
+                    <option value="MW1">M/W 10:30-11:20</option>
+                    <option value="MW2">M/W 12:00-12:50</option>
+                    <option value="MW3">M/W 3:00-3:50</option>
+                    <option value="MW4">M/W 4:30-5:20</option>
+                    <option value="TT1">T/Th 10:30-11:20</option>
+                    <option value="TT2">T/Th 12:00-12:50</option>
+                    <option value="TT3">T/Th 3:00-3:50</option>
+                    <option value="TT4">T/Th 4:30-5:20</option>
+                </select>
+
                 <label className='formLabel' htmlFor="instructorName">Instructor Name:</label>
                 <input
                     type="text"
@@ -77,7 +99,6 @@ const IssueSubmissionForm = () => {
                     className='formControl'
                     value={instructorName}
                     onChange={(e) => setInstructorName(e.target.value)}
-                    required
                 />
 
                 <label className='formLabel' htmlFor="issueType">Select Issue Type:</label>
@@ -89,10 +110,9 @@ const IssueSubmissionForm = () => {
                     required
                 >
                     <option value="" disabled>Select an issue</option>
-                    <option value="sponsorNotCommunicating">Sponsor Not Communicating</option>
-                    <option value="teamMemberNotContributing">Team Member Not Contributing</option>
-                    <option value="delaysOutOfControl">Project Setbacks</option>
-                    <option value="sponsorBeingRude">Sponsor Being Unprofessional</option>
+                    <option value="sponsorIssue">Issues with Sponsor</option>
+                    <option value="teamIssue">Issues within the Team</option>
+                    <option value="assignmentIssue">Issues with Assignments or Grading (Including Extensions)</option>
                     <option value="other">Other</option>
                 </select>
 
@@ -116,7 +136,7 @@ const IssueSubmissionForm = () => {
                     placeholder="Enter names, separated by commas"
                 />
 
-                <button className='submitButton' type="submit">Submit Issue</button>
+                <button className='submitButton' type="submit">Submit Request</button>
             </form>
         </div>
     );
