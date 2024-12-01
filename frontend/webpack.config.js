dc3adb3c1b7590220a90882dfb376ddb1c371a74
kevin <ktang2332@gmail.com>
2024-11-30 18:37:11 -0700

    change default api url

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     new webpack.DefinePlugin({
       "process.env": JSON.stringify({
         REACT_APP_API_BASE_URL:
-          process.env.REACT_APP_API_BASE_URL || "http://localhost:3302",
+          process.env.REACT_APP_API_BASE_URL || "https://ticketing-system-api-uht1.onrender.com",
       }),
     }),
   ],
