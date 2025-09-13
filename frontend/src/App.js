34a4258e5b09c2c87900dbdef1c8370e4eab557a
Avery-Powers <aapcalculus@gmail.com>
2025-09-12 17:30:20 -0700

    persistent login feature update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ import Registration from "./pages/Registration/Registration";
 import Profile from "./pages/Profile/Profile";
 import StudentTickets from "./pages/StudentTickets/StudentTickets";
 import InstructorTickets from "./pages/InstructorTickets/InstructorTickets";
-import InstructorProfile from "./pages/InstructorProfile/InstructorProfile"; //added 3/22/25 along with creation of instructor profile
+import InstructorProfile from "./pages/InstructorProfile/InstructorProfile"; 
 
 function App() {
   return (
@@ -50,7 +50,7 @@ function App() {
         <Route path="/tasettings" element={<TASettings />} />
         <Route path="/studentsettings" element={<StudentSettings />} />
         <Route path="/ticketview" element={<TicketView />} />
-	<Route path="/instructorprofile" element={<InstructorProfile />} /> //added 3/22/25 along with creation of instructor profile
+	      <Route path="/instructorprofile" element={<InstructorProfile />} /> 
         {/* Testing Pages */}
 
         {/* Change user_id to the user's id */}
