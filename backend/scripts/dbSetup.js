d3a5a988f16f0f099bf5c06ec7d1cabd6bd309b3
davidrivera593 <131416582+davidrivera593@users.noreply.github.com>
2025-10-29 17:10:26 -0700

    Changes to be committed: 	modified:   backend/config/config.json 	modified:   package.json

--- backend/scripts/dbSetup.js
+++ backend/scripts/dbSetup.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 // Initialize connection pool
 const pool = new Pool({
   connectionString:
-  	"postgresql://ticketing_user:ticketing_pass@127.0.0.1:5432/ticketing_system",
+  	"postgresql://myuser:Lucky01!@127.0.0.1:5432/mydatabase",
 	//ssl: { rejectUnauthorized: false }, // Only needed if using SSL in production
 });
 
